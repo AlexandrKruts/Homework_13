88089dda4c2e4eaabdf65a0917294d6f60c59bc1
AlexandrKruts <kruts704@gmail.com>
2022-10-14 21:30:03 +0300

    corrected task

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 "use strict";
 
-let API_URL = fetch("https://jsonplaceholder.typicode.com/users")
+ fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
  
   .then((result) => {
-    let filteredUsers = result.filter((item) => item.company.name === "Johns Group");
-    console.log(filteredUsers);
+    let findUsers = result.find((item) => item.company.name === "Johns Group");
+    let {name} = findUsers;
+    console.log(name);
   })
   .catch((error) => console.error(error));
+
